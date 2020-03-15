189bbfc9fd2eb4103f8aafe8d702bc7d95fbf503
Ailton Martins <pinho123@gmail.com>
2020-03-15 15:03:51 -0300

    Criado a migration movies e tags

---
+++ api/database/migrations/2020_03_15_174005_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('file');
+            $table->bigInteger('file_size')->unsigned()->nullable();
+            $table->bigInteger('user')->unsigned();
+            $table->foreign('user')->references('id')
+                                   ->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
