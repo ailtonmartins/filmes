7c6fb412f6ee738273c33719f37d7cb49cd43306
Ailton Martins <pinho123@gmail.com>
2020-03-15 17:10:38 -0300

    Criado cadastro simples do Movie sem a validação do arquivo

--- api/app/Http/Controllers/MovieController.php
+++ api/app/Http/Controllers/MovieController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Facades\Auth;
 
 class MovieController extends Controller
 {
@@ -25,9 +27,29 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
+          
           $data = $request->all();
 
-        return response()->json( $data, 200); 
+          $validator = Validator::make($data, [
+                'name' => 'required',
+                'file' => 'required'
+          ]);
+
+          if ($validator->fails()) {
+                return response()->json([
+                    'message'   => 'Validation Failed',
+                    'errors'    => $validator->errors()->all()
+                ], 422);
+          }
+
+          $user = Auth::user();
+
+          $movie = new Movie();
+          $movie->fill($data);
+          $movie->user = $user->id;
+          $movie->save();
+
+        return response()->json( $movie, 200); 
     }
 
     /**
